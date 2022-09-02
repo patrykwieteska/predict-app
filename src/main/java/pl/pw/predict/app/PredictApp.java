fbba2c92f22200efa04ad3f810b362843e5a2c07
patrykwieteska <patrykwieteskapw@gmail.com>
2022-09-02 18:15:52 +0200

    Initial commit

---
+++ src/main/java/pl/pw/predict/app/PredictApp.java
@@ -0,0 +1,13 @@
+package pl.pw.predict.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PredictApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run( PredictApp.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pw/predict/app/PredictAppTests.java
@@ -0,0 +1,13 @@
+package pl.pw.predict.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PredictAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
